24eb1aeded76986acbf1fff5ee460b694cab18d6
abdurahman <abdurahmankizhakkail@outlook.com>
2022-09-03 23:05:22 +0530

    remove dependecies folder

--- target/release/deps/crypto_common-b941821733766e4e.d
+++
@@ -1,7 +0,0 @@
-C:\Users\abdur\coding\rust\argon2\deno_argon2\target\release\deps\crypto_common-b941821733766e4e.rmeta: C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\crypto-common-0.1.6\src\lib.rs
-
-C:\Users\abdur\coding\rust\argon2\deno_argon2\target\release\deps\libcrypto_common-b941821733766e4e.rlib: C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\crypto-common-0.1.6\src\lib.rs
-
-C:\Users\abdur\coding\rust\argon2\deno_argon2\target\release\deps\crypto_common-b941821733766e4e.d: C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\crypto-common-0.1.6\src\lib.rs
-
-C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\crypto-common-0.1.6\src\lib.rs:
--- target/release/deps/lazy_static-2499761fa9054f35.d
+++
@@ -1,8 +0,0 @@
-C:\Users\abdur\coding\rust\argon2\deno_argon2\target\release\deps\lazy_static-2499761fa9054f35.rmeta: C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
-
-C:\Users\abdur\coding\rust\argon2\deno_argon2\target\release\deps\liblazy_static-2499761fa9054f35.rlib: C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
-
-C:\Users\abdur\coding\rust\argon2\deno_argon2\target\release\deps\lazy_static-2499761fa9054f35.d: C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs
-
-C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\lib.rs:
-C:\Users\abdur\.cargo\registry\src\github.com-1ecc6299db9ec823\lazy_static-1.4.0\src\inline_lazy.rs:
